d701b5182de177d43e75c68de97e657a54faaf66
michaelt <what_is_it_to_do_anything@yahoo.com>
2014-02-15 10:01:48 -0500

    use new text-stream-decoding

--- Pipes/Text/Internal.hs
+++
@@ -1,7 +0,0 @@
-module Pipes.Text.Internal
-    (module Pipes.Text.Internal.Codec
-    , module Pipes.Text.Internal.Decoding
-    ) where
-
-import Pipes.Text.Internal.Codec
-import Pipes.Text.Internal.Decoding
