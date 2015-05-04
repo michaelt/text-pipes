5e387e52ab52117a1b245508232cd04d79fff47d
michaelt <what_is_it_to_do_anything@yahoo.com>
2015-05-03 21:38:59 -0400

    wibble examples

--- examples/lines_url.hs
+++ examples/lines_url.hs
@@ -23,15 +23,17 @@ main = do
                 -- "http://www.gutenberg.org/files/10/10-h/10-h.htm"
   withManager tlsManagerSettings $ \m ->
     withHTTP req m $ \resp ->  void $ runEffect $ 
-         number_lines_of (responseBody resp ^. utf8 . lines) >-> toHandle IO.stdout
+      number_lines_of (responseBody resp ^. utf8 . lines) >-> toHandle IO.stdout
 
-number_lines_of :: Monad m => FreeT (Producer Text m) m bad -> Producer Text m bad 
+number_lines_of :: Monad m => FreeT (Producer Text m) m r -> Producer Text m r
 number_lines_of  = number_loop (1 :: Int) where
-  number_loop n freeProducers = do
-        freeProducer <- lift $ runFreeT freeProducers
-        case freeProducer of
-          Pure badbytes -> do yield $ T.pack "\n"
-                              return badbytes -- these could be inspected ... 
-          Free p -> do yield $ T.pack ("\n" ++ show n ++ "  ") 
-                       nextFreeProducers <- p
-                       number_loop (n+1) nextFreeProducers
+  number_loop n free = do
+    fproducer <- lift $ runFreeT free
+    case fproducer of
+      Pure badbytes -> do 
+        yield $ T.pack "\n"
+        return badbytes -- these could be inspected ... 
+      Free p -> do 
+        yield $ T.pack ("\n" ++ show n ++ "  ") 
+        next_free <- p
+        number_loop (n+1) next_free
